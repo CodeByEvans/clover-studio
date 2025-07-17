4458382fca3a3f828eb85650edc404738bf21bba
EvansJLR <evansjavierlr@gmail.com>
2025-07-17 14:34:52 +0200

    fix(lib/api/categoryApy.ts): fix relatie url error

--- src/lib/api/categoryApi.ts
+++ src/lib/api/categoryApi.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const getCategories = () =>
-  axios.get("/api/categories").then((res) => res.data);
+  axios.get(`${BASE_URL}/api/categories`).then((res) => res.data);
+
 export const getCategory = (id: string) =>
-  axios.get(`/api/categories/${id}`).then((res) => res.data);
+  axios.get(`${BASE_URL}/api/categories/${id}`).then((res) => res.data);
