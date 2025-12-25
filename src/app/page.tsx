a8f6d953ab0895a82c2a02b0e4c0e4e033998a60
Evans Javier <evans@Mac-mini-de-Evans.local>
2025-12-25 23:35:39 +0100

    feat: add floating whatsapp icon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use server";
 import Hero from "./_components/hero";
-import FeaturedCollection from "./_components/featured-collection";
+import CollectionsSection from "./_components/CollectionsSection";
 import Testimonials from "./_components/testimonials";
 import FeaturesSection from "./_components/FeaturesSection";
+import FloatingWhatsappIcon from "@/components/FloatingWhatsappIcon";
 
 export default async function Home() {
   return (
     <>
       <Hero />
-      <FeaturedCollection />
+      <CollectionsSection />
       <FeaturesSection />
       <Testimonials />
+      <FloatingWhatsappIcon />
     </>
   );
 }
